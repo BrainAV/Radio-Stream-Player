7542afb21576db28e1a5a1a9e9254ac4feaee773
Jason Brain <jason.brain@hotmail.com>
2026-03-01 21:21:12 -0500

    Update stations.js

--- stations.js
+++ stations.js
@@ -6,7 +6,7 @@ export const stations = [
     { name: "Psychedelic FM", url: "https://s2.radio.co/s2696f08b5/listen", genre: "Psytrance", country: "Netherlands" },
     { name: "Melodic House Radio", url: "https://stream1-technolovers.radiohost.de/melodic-house-techno", genre: "Melodic House", country: "Germany" },
     { name: "psyradio * fm - progressive", url: "http://65.109.32.21:8010/stream", genre: "Progressive Psy", country: "Germany" },
-    { name: "Party~Vibe | Psychedelic Trance Radio", url: "http://94.130.242.5:8010/stream3", genre: "Psytrance", country: "UK" },
+    { name: "Party~Vibe | Psychedelic Trance Radio", url: "https://www.partyvibe.com:8062/;listen.pls?sid=1", genre: "Psytrance", country: "UK" },
     { name: "DI.FM - Vocal Trance", url: "https://listen.di.fm/vocaltrance", genre: "Vocal Trance", country: "USA" },
     { name: "Radiozora - Psytrance", url: "https://2026.radiozora.fm/stream/", genre: "Psytrance", country: "Hungary" },
     { name: "A State of Trance (ASOT)", url: "https://streaming.radio.co/s0cf4c8c63/listen", genre: "Trance", country: "Netherlands" }
