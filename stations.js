6d8431912af6eeea006dab817120bec50787e5c7
Jason Brain <jason.brain@hotmail.com>
2026-03-05 00:47:46 -0500

    Update stations.js

--- stations.js
+++ stations.js
@@ -4,7 +4,6 @@ export const stations = [
     { name: "Psyndora Psytrance", url: "https://cast.magicstreams.gr/sc/psyndora/stream", genre: "Psytrance", country: "Greece" },
     { name: "DMT-FM", url: "https://dc1.serverse.com/proxy/ywycfrxn/stream", genre: "Psytrance", country: "UK" },
     { name: "BOM Psytrance Radio", url: "https://strm112.1.fm/psytrance_mobile_mp3?aw_0_req.gdpr=true", genre: "Psytrance", country: "Switzerland" },
-    { name: "Psychedelic FM", url: "https://s2.radio.co/s2696f08b5/listen", genre: "Psytrance", country: "Netherlands" },
     { name: "Melodic House Radio", url: "https://stream1-technolovers.radiohost.de/melodic-house-techno", genre: "Melodic House", country: "Germany" },
     { name: "psyradio * fm - progressive", url: "http://65.109.32.21:8010/stream", genre: "Progressive Psy", country: "Germany" },
     { name: "Party~Vibe | Psychedelic Trance Radio", url: "https://www.partyvibe.com:8062/;listen.pls?sid=1", genre: "Psytrance", country: "UK" },
